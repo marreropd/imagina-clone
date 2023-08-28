b1becd8c05fcb3de7fffc6220ffcc37afbcc4270
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-08-28 16:54:12 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Landing } from "./components/Landing/Landing";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Landing />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+import { NavBar } from "../NavBar/NavBar";
+
+export const Landing = () => {
+  return (
+    <div className="w-screen h-screen bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')]">
+      <NavBar />
+    </div>
+  );
+};
