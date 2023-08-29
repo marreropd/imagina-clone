b95d15cd0eeb7acf007433d90ead978c214cf33a
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-08-29 12:26:22 +0200

    add styles to landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,11 +4,16 @@ export const Landing = () => {
   return (
     <>
       <NavBar />
-
       <div className="w-screen background-image h-[28rem] flex place-items-center justify-center bg-center bg-bottom bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')]">
         <div className="text-6xl text-white place-items-center flex flex-col justify-center">
           <h1 className="font-bold">Próximas convocatorias</h1>
-          <h2 className="font-bold">Modalidad Online</h2>{" "}
+          <div
+            className="text-center inset-y-1/2 left-1/2 right-2/4 py-[10px] px-[50px] bg-repeat-y
+          bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/strokePruebaOnlinewebp.webp')]"
+          >
+            <h2 className="font-semibold">Modalidad Online</h2>
+          </div>
+
           <div className="text-sm w-3/4	text-center mt-10">
             <p>
               A continuación te mostramos las convocatorias de nuestros cursos
