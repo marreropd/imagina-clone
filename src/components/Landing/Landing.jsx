854480a4dc64899a892296e07ef6dec58e060df0
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-08-29 12:08:58 +0200

    add styles to landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,8 +2,22 @@ import { NavBar } from "../NavBar/NavBar";
 
 export const Landing = () => {
   return (
-    <div className="w-screen h-screen bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')]">
+    <>
       <NavBar />
-    </div>
+
+      <div className="w-screen background-image h-[28rem] flex place-items-center justify-center bg-center bg-bottom bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')]">
+        <div className="text-6xl text-white place-items-center flex flex-col justify-center">
+          <h1 className="font-bold">Próximas convocatorias</h1>
+          <h2 className="font-bold">Modalidad Online</h2>{" "}
+          <div className="text-sm w-3/4	text-center mt-10">
+            <p>
+              A continuación te mostramos las convocatorias de nuestros cursos
+              online. En caso de querer formar a un grupo de trabajadores, te
+              recomendamos nuestra modalidad Aula Virtual Personalizada
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
