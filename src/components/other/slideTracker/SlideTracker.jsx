7ea3055342e0df931925542ffa716eba450931c9
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-08-30 19:33:51 +0200

    create slide tracker component

---
+++ src/components/other/slideTracker/SlideTracker.jsx
@@ -0,0 +1,123 @@
+import "./slideTrackerCss.scss";
+export const SlideTracker = () => {
+  return (
+    <div>
+      <div className="slider  w-screen">
+        <div className="slide-track">
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
