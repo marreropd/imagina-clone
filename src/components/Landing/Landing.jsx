f6de15945dc143db39e433057e6d326cd27c317d
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-08-31 20:17:28 +0200

    add link to landing section

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 import { NavBar } from "../NavBar/NavBar";
 import { SlideTracker } from "../other/slideTracker/SlideTracker";
+import { AdvantagesSection } from "../AdvantagesSecttion/AdvantagesSection";
 
 export const Landing = () => {
   return (
     <>
       <NavBar />
-      <section className="w-screen background-image h-[28rem] flex place-items-center justify-center bg-center bg-bottom bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')]">
+      <section className="background-image h-[28rem] flex place-items-center justify-center bg-center bg-bottom bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/backgroundHero05.webp')] ">
         <div className="text-6xl text-white place-items-center flex flex-col justify-center">
-          <h1 className="font-bold">Próximas convocatorias</h1>
+          <h1 className="font-bold">Próximas Convocatorias</h1>
           <div
             className="text-center mt-2 inset-y-1/2 left-1/2 right-2/4 py-[10px] px-[50px] bg-repeat-y
           bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/strokePruebaOnlinewebp.webp')]"
@@ -15,11 +17,14 @@ export const Landing = () => {
             <h2 className="font-semibold">Modalidad Online</h2>
           </div>
 
-          <div className="text-sm w-3/4	text-center mt-10">
+          <div className="text-base w-3/4	text-center mt-10">
             <p>
               A continuación te mostramos las convocatorias de nuestros cursos
               online. En caso de querer formar a un grupo de trabajadores, te
-              recomendamos nuestra modalidad Aula Virtual Personalizada
+              recomendamos nuestra modalidad{" "}
+              <Link className="font-semibold underline underline">
+                Aula Virtual Personalizada
+              </Link>
             </p>
           </div>
         </div>
@@ -27,6 +32,10 @@ export const Landing = () => {
 
       <section>
         <SlideTracker />
+      </section>
+
+      <section className="grid place-items-center h-96 px-24">
+        <AdvantagesSection />
       </section>
     </>
   );
