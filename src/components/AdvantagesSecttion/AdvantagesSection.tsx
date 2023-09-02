887c20e141966d9d44c3f6196070d2b4fff9f97b
Pablo Marrero <pablomarrerofernandez1@gmail.com>
2023-09-02 14:47:59 +0200

    fix warnings nested p

--- src/components/AdvantagesSecttion/AdvantagesSection.tsx
+++ src/components/AdvantagesSecttion/AdvantagesSection.tsx
@@ -14,23 +14,25 @@ export const AdvantagesSection = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="p-3 border rounded-lg shadow h-min rounded">
-          <p className="font-normal text-gray-700 dark:text-gray-400">
+          <div className="font-normal text-gray-700 dark:text-gray-400">
             {advantages.map((advantages: IAdvatages) => {
               return (
-                <div className="flex p-6">
-                  <div className="text-start text-violet-800 -mt-2 mr-1">
+                <span className="flex p-6" key={advantages.title}>
+                  <span className="text-start text-violet-800 -mt-2 mr-1">
                     {advantages.icon as ReactNode}
-                  </div>
-                  <div>
-                    <h2 className="font-bold text-black text-lg">
+                  </span>
+                  <span>
+                    <p className="font-bold text-black text-lg">
                       {advantages.title}
-                    </h2>
-                    <div className=" text-black">{advantages.description}</div>
-                  </div>
-                </div>
+                    </p>
+                    <span className=" text-black">
+                      {advantages.description}
+                    </span>
+                  </span>
+                </span>
               );
             })}
-          </p>
+          </div>
         </div>
         <div className="border bg-center rounded-lg shadow h-min[610px] self-center max-w-screen-md bg-cover bg-no-repeat h-[610px] w-[510px] bg-[url('https://imaginaformacion.com/plasmic/imagina_formacion_landing_components/images/onlineVentajaswebp.webp')]	"></div>
       </div>
